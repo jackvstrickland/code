2eae8558fa5748999af716e59654aece56390186
jackvstrickland <jackvstrickland@icloud.com>
2017-01-07 20:00:28 -0600

    fixed hourglass sum
    the function was summing wrong

--- hacker-rank/2D-array/hourglass.py
+++ hacker-rank/2D-array/hourglass.py
@@ -2,15 +2,12 @@
 
 import sys
 
-def getHourglassSum(arr, h_col, h_row):
-	total = 0
+def getHourglassSum(arr, h_row, h_col):
+	total = arr[h_row+1][h_col+1]
 	for y in range(0,3,2):
 		for x in range(h_col, h_col+3):
 			total += arr[h_row+y][x]
-	total += arr[h_col+1][h_row+1]
 	return total
-
-
 
 arr = []
 sums = []
@@ -18,10 +15,11 @@ for arr_i in range(6):
    arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
    arr.append(arr_t)
 
-for row in range(0,4):
-	for col in range(0,4):
-		sums.append(getHourglassSum(arr, col, row))
+# hg_max, hg_nextMax = None
 
-print(arr)
+for row in range(0,4):
+ 	for col in range(0,4):
+ 		sums.append(getHourglassSum(arr, row, col))
+
 print(sums)
 
